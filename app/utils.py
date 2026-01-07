70384cba9ba4e3cffc6fe74901cc5d737322850c
joannes59 <joannes.landy@gmail.com>
2026-01-07 18:41:13 +0100

    test github pytest path v3

--- app/utils.py
+++ app/utils.py
@@ -7,18 +7,13 @@ def load_model(file_name):
     Charge le modèle Machine Learning depuis le dossier /model.
     Retourne le modèle chargé ou raise en cas d'erreur.
     """
-    project_root = '?'
-
-    if os.path.exists('model/' + file_name):
-        model_path = 'model/' + file_name
-    else:        
-        project_root = Path.cwd()
-        model_path = os.path.join(project_root, "model", file_name)
+    project_root = Path.cwd()      
+    model_path = os.path.join(project_root, "model", file_name)
 
     try:
         model = joblib.load(model_path)
         return model
 
     except Exception as e:
-        raise ValueError(f"model_path: {model_path}\n{e}")
+        raise ValueError(f"model_path: {model_path}\nproject_root:{project_root}_n{e}")
         
