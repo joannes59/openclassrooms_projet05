bb3ee92178e10910c7d632b9315bb896430090a8
joannes59 <joannes.landy@gmail.com>
2026-01-07 15:19:44 +0100

    Change path join v2

--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -7,12 +7,13 @@ from app import preprocess
 import pytest
 import json
 
+encoder = utils.load_model("onehotencoder.joblib")
+scaler = utils.load_model("scaler.joblib")
+model = utils.load_model("model.joblib")
+
 
 def test_load_encoder():
     """ check the configuration model load """
-    encoder = utils.load_model("onehotencoder.joblib")
-    scaler = utils.load_model("scaler.joblib")
-    model = utils.load_model("model.joblib")
 
     assert encoder is not None
     assert scaler is not None
@@ -32,10 +33,6 @@ def data_example():
 def test_encode(data_example):
     """ check the response of encode data """
         
-    encoder = utils.load_model("onehotencoder.joblib")
-    scaler = utils.load_model("scaler.joblib")
-    model = utils.load_model("model.joblib")
-
     if encoder and scaler and model:
         x_exemple_scaled = preprocess.encode(data_example)
         
