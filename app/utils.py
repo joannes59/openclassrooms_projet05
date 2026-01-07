8521aa41994b2b8c457a030814069c059fa5f682
joannes59 <joannes.landy@gmail.com>
2026-01-07 18:32:11 +0100

    test github pytest path v2

--- app/utils.py
+++ app/utils.py
@@ -5,13 +5,14 @@ from pathlib import Path
 def load_model(file_name):
     """
     Charge le modèle Machine Learning depuis le dossier /model.
-    Retourne le modèle chargé ou None en cas d'erreur.
+    Retourne le modèle chargé ou raise en cas d'erreur.
     """
+    project_root = '?'
 
     if os.path.exists('model/' + file_name):
         model_path = 'model/' + file_name
     else:        
-        project_root = Path(__file__).resolve().parents[1]
+        project_root = Path.cwd()
         model_path = os.path.join(project_root, "model", file_name)
 
     try:
