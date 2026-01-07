677569e3bbb32686b943091f4bc91de57e26bcbe
joannes59 <joannes.landy@gmail.com>
2026-01-07 19:04:29 +0100

    test github pytest path v7

--- app/utils.py
+++ app/utils.py
@@ -7,7 +7,7 @@ def load_model(file_name):
     Charge le modèle Machine Learning depuis le dossier /model.
     Retourne le modèle chargé ou raise en cas d'erreur.
     """
-    project_root = Path.cwd()      
+    project_root = Path(__file__).resolve().parent.parent
     model_path = str(project_root) + "/model/" + file_name
 
     try:
