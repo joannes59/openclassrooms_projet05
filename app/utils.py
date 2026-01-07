15693b6a4508d6725579bf88c6a888d70e71fea3
joannes59 <joannes.landy@gmail.com>
2026-01-07 15:49:45 +0100

    Change path join v3

--- app/utils.py
+++ app/utils.py
@@ -6,11 +6,13 @@ def load_model(file_name):
     Charge le modèle Machine Learning depuis le dossier /model.
     Retourne le modèle chargé ou None en cas d'erreur.
     """
-
-    model_path = os.path.join(
-        "model",
-        file_name
-    )
+    docker_root = '/code'
+    if os.path.exists(docker_root):
+        root_code = docker_root
+    else:
+        root_code = os.path.dirname(__file__) + '/..'
+        
+    model_path = os.path.join(root_code, "model", file_name)
 
     try:
         model = joblib.load(model_path)
